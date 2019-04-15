ca6034a0290c6389cccbcdafa992af81036ff42e
Binyamin Darshan Hcohen <bennydarshan@linuxrocks.online>
2019-04-15 10:27:43 +0300

    Added missing scripts, again from podcasts

---
+++ scripts/cargo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+export OUTPUT="$2"
+export CARGO_TARGET_DIR="$3"/target
+export CARGO_HOME="$CARGO_TARGET_DIR"/cargo-home
+export PROFILE="$4"
+
+TARGET=debug
+ARGS=()
+
+if test "$PROFILE" != ".Devel"; then
+    echo "RELEASE MODE"
+    ARGS+=('--release')
+    TARGET=release
+fi
+
+if test -d vendor; then
+    echo "VENDORED"
+    ARGS+=('--frozen')
+fi
+
+cargo build ${ARGS[@]} --manifest-path="$1"/Cargo.toml -p titanium &&
+cp "$CARGO_TARGET_DIR"/${TARGET}/titanium "$OUTPUT"
+
